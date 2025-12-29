5dae1a27337a66e9c1878f8fc016fcc9aac5cd38
Aniket03g <aniket03g@gmail.com>
2025-12-29 12:09:06 +0530

    Add nginx.conf for frontend and update Docker/config files

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   
   server: {
     port: 4321,
